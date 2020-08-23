4613a8157148e3780f770b3cbbb7335858f8335d
parinvasudevan <68951819+parinvasudevan@users.noreply.github.com>
2020-08-23 15:02:13 +0530

    Add files via upload

---
+++ redzone.js
@@ -0,0 +1,18 @@
+class redzone {
+    constructor(x,y,width,height){}
+    this.body=Bodies.rectangle(250,390,50,50);
+    this.width = 50;
+    this.height = 50;
+    this.x= 250;
+    this.y= 390;
+
+ }
+  display(){
+
+ var pos =this.body.position;
+  rectMode(CENTER);
+  fill("red");
+  
+
+  }
+ 
